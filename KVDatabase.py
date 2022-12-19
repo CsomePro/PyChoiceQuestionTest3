bfc14d83d97e19347174f7a31c1f7ec7538ec808
Csome <csome@m.scnu.edu.cn>
2022-12-19 13:21:53 +0800

    搭建好了redis服务器，并开发了底层redis支持

--- KVDatabase.py
+++ KVDatabase.py
@@ -4,6 +4,26 @@ import json
 import typing
 
 
+class BankDatabase:
+    """
+    题库数据库抽象
+    switch() 切换题库
+        get_bank() 获取题库
+    """
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def switch(self, *args, **kwargs) -> None:
+        pass
+
+    def get_bank(self) -> list:
+        pass
+
+    def create_bank(self, *args, **kwargs):
+        pass
+
+
 class KVDatabase:
     """
     数据库抽象类
@@ -12,6 +32,7 @@ class KVDatabase:
     read_all(int) 获取一定数量的数据序列
     save() load() 加载以及保存数据库
     """
+
     def __init__(self, *args, **kwargs):
         pass
 
@@ -42,10 +63,11 @@ class KVDatabase:
     @staticmethod
     def getKVDatabase(mode='None', *args, **kwargs):
         if mode == 'question':
-            return KVDatabase_json(*args, **kwargs)
+            # return KVDatabase_json(*args, **kwargs)
+            return KVDatabase_redis(*args, **kwargs)
 
 
-class KVDatabase_json(KVDatabase, ScheduleTask):
+class KVDatabase_json(KVDatabase, ScheduleTask, BankDatabase):
     def __init__(self, json_file, *args, **kwargs):
         super().__init__(*args, **kwargs)
         super(KVDatabase, self).__init__(5)
@@ -104,9 +126,99 @@ class KVDatabase_json(KVDatabase, ScheduleTask):
         with open(self.json_file, "w", encoding='utf-8') as f:
             json.dump(self.database, f)
 
+    def switch(self, file=None):
+        self.open_json(file)
+
+    def get_bank(self) -> list:
+        import os
+        dir_bank = '.\\bank'
+        return list(
+            map(lambda x: os.path.join(dir_bank, x), filter(lambda x: x.endswith('.json'), os.listdir(dir_bank))))
+
+    def create_bank(self, file):
+        with open(file, "w") as f:
+            f.write("{}")
+        self.open_json(file)
+
+
+class KVDatabase_redis(KVDatabase, ScheduleTask, BankDatabase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        super(KVDatabase, self).__init__(10)
+        import redis
+        self.redis = self.connect()
+        self.last_idx: int = max([0] + self.get_index())
+
+    @staticmethod
+    def connect():
+        import redis
+        import os
+        return redis.Redis(host=os.environ.get('REDIS_IP'),
+                           port=16379,
+                           decode_responses=True,
+                           password=os.environ.get('REDIS_PASSWORD'))
+
+    def reconnect(self):
+        self.redis.close()
+        self.redis = self.connect()
+
+    def do_check(self):
+        print("check")
+        if not self.redis.ping():
+            self.reconnect()
+
+    def get_index(self) -> typing.List:
+        return list(map(lambda x: self.key2idx(x), filter(lambda x: x.startswith('Q_'), self.redis.keys())))
+
+    def read_all(self, limit=-1) -> typing.List[typing.Tuple[int, Question]]:
+        key = self.get_index()
+        return list(map(lambda x: (x, self.str2question(self.redis.get(self.idx2key(x)))), key))
+
+    def read(self, idx) -> Question:
+        k = self.idx2key(idx)
+        return self.str2question(self.redis.get(k))
+
+    def delete(self, idx) -> int:
+        self.redis.delete(self.idx2key(idx))
+        return idx
+
+    def update(self, idx, q: Question) -> int:
+        self.redis.set(self.idx2key(idx), self.question2str(q))
+        return idx
+
+    def create(self, q: Question) -> int:
+        self.last_idx += 1
+        k = self.idx2key(self.last_idx)
+        self.redis.set(k, self.question2str(q))
+        return self.last_idx
+
+    def save(self, *args, **kwargs):
+        pass
+
+    def load(self, *args, **kwargs):
+        pass
+
+    @staticmethod
+    def idx2key(idx) -> str:
+        return f"Q_{idx:08d}"
+
+    @staticmethod
+    def key2idx(s) -> int:
+        return int(s[2:])
+
+    @staticmethod
+    def question2str(q: Question) -> str:
+        return json.dumps(q.to_tuple())
+
+    @staticmethod
+    def str2question(s) -> Question:
+        surface, ans, options, t = json.loads(s)
+        return Question(problemSurface=surface, answer=ans, options=options, pType=t)
+
 
 if __name__ == '__main__':
-    kvj = KVDatabase_json("test.json")
+    # kvj = KVDatabase_json("bank/test.json")
+    kvj = KVDatabase_redis()
     # q1 = Question("1+1=?", [1, 0], ["2", "1"], QuestionType.SINGLE)
     # q2 = Question("1+1<=?", [1, 0, 1], ["2", "1", "3"], QuestionType.MULTIPLE)
     # q3 = Question("1+1=2?", [1, 0], ["yes", "no"], QuestionType.JUDGEMENTAL)
@@ -114,5 +226,6 @@ if __name__ == '__main__':
     # for q in ql:
     #     kvj.create(q)
     # kvj.save_json()
-    print(kvj.read(1))
-    print(kvj.get_index())
+    # print(kvj.read(3))
+    # print(kvj.get_index())
+    # print(kvj.get_bank())
